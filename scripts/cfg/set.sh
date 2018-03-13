0774d6deac2fde3bc5e56b2b3b02d1a4b3aaf082
Alex <alexzhangs@gmail.com>
2018-03-13 17:17:42 +0800

    Update cfg/set.sh, check parameters

--- scripts/cfg/set.sh
+++ scripts/cfg/set.sh
@@ -11,14 +11,23 @@
 #?   Updated profile.
 #?
 function set () {
-    local name=${1:-default}
+    local name=$1
     local base_dir property n
 
     base_dir=$(dirname "$(xsh /file/symblink "$0")")
     . "${base_dir}/config.conf"
 
+    if [[ -z ${name} ]]; then
+        printf "ERROR: profile name null or not set.\n" >&2
+        return 255
+    fi
+
     n=2  # profile properties started at $2
     for property in "${AWS_CFG_PROPERTIES[@]}"; do
+        if [[ -z ${property#*.} || -z ${!n} ]]; then
+            continue
+        fi
+
         aws configure set "${property#*.}" "${!n}" --profile "${name}"
         n=$((n+1))
     done
