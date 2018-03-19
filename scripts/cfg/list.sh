79c22f489e852a9398496d65be83348716efcc39
Alex <alexzhangs@gmail.com>
2018-03-19 19:17:16 +0800

    Update cfg/list.sh: support field header

--- scripts/cfg/list.sh
+++ scripts/cfg/list.sh
@@ -8,13 +8,57 @@
 #?
 function list () {
     local result pattern
+    local i sep str properties property sensitive base_dir
 
-    result=$(xsh aws/cfg/get \
-                | xsh /file/mask -d, -f4 -c1-36 -x \
-                | column -s, -t)
+    base_dir=$(dirname "$(xsh /file/symblink "$0")")
+    . "${base_dir}/config.conf"
 
-    pattern=$(echo "${result}" \
-                  | awk '$1 == "default" {OFS="[ ]+"; $1 = ".+"; $4 = ".+"; print $0}')
+    properties=(
+        "profile"
+        "${AWS_CFG_PROPERTIES[@]}"
+    )
+
+    i=1
+    for property in "${properties[@]}"; do
+        if [[ ${property#*.} == 'aws_secret_access_key' ]]; then
+            sensitive=$i
+        fi
+        i=$((i+1))
+    done
+
+    result=$(
+        {
+            sep=""
+            for property in "${properties[@]}"; do
+                str=${property#*.}
+                printf "${sep}${str}"
+                sep=","
+            done
+            printf "\n"
+
+            sep=""
+            for property in "${properties[@]}"; do
+                str=${property#*.}
+                printf "%s" "${sep}$(xsh /string/repeat/1 '-' ${#str})"
+                sep=","
+            done
+            printf "\n"
+
+            xsh aws/cfg/get \
+                | xsh /file/mask -d, -f4 -c1-36 -x
+        } | column -s, -t)
+
+    pattern=$(
+        echo "${result}" \
+            | awk -v secret_field=${sensitive} \
+                  '$1 == "default" {
+                      OFS="[ ]+"
+                      $1 = ".+"
+                      if (secret_field) {
+                         $secret_field = ".+"
+                      }
+                      print $0
+                  }')
 
     echo "${result}" | xsh /file/mark -p "^${pattern}$"  # highlight activated profile
 }
