f67c664f8bf65970f7833ed68bd936a2563bdbe7
Alex <alexzhangs@gmail.com>
2019-10-26 02:23:46 +0800

    update: ec2/desc.sh: add filter, query and output format to the interface

--- functions/ec2/desc.sh
+++ functions/ec2/desc.sh
@@ -2,8 +2,86 @@
 #?   Describe EC2 instances.
 #?
 #? Usage:
-#?   @desc <INSTANCE_ID> [...]
+#?   @desc
+#?     [-f FILTER] [...]
+#?     [-q QUERY]
+#?     [-o json | text | table]
+#?
+#? Options:
+#?   [-f FILTER] [...]
+#?
+#?   The filters, syntax: `NAME=VALUE`.
+#?   The valid NAME and VALUE, see the `--filters` section of `aws ec2 describe-instances help`.
+#?   This option can be used multiple times to narrow results.
+#?
+#?   [-q QUERY]
+#?
+#?   A JMESPath query to use in filtering the response data.
+#?   See spec of JMESPath: http://jmespath.org/specification.html
+#?   Give the query without the part: `Reservations[*][]`.
+#?
+#?   [-o json | text | table]
+#?
+#?   The formatting style for command output.
+#?   The default is `json`.
+#?
+#? Example:
+#?   # list unteminated instances, output public IP address and the tag value of `Name`
+#?   $ @desc -f instance-state-name=pending,running,stopping,stopped \
+#?           -q "Instances[*].[PublicIpAddress,Tags[?Key=='Name'].Value]"
+#?   [
+#?       [
+#?           [
+#?               "54.180.56.131",
+#?               [
+#?                   "MyInstance"
+#?               ]
+#?           ]
+#?       ]
+#?   ]
+#?
+#?   # list unteminated instances, output public IP address and the value of tag `Name`,
+#?   # filter out the instances without a tag `Name` or wihtout a value for tag `Name`,
+#?   # the `[]` around the tag is stripped.
+#?   $ @desc -f instance-state-name=pending,running,stopping,stopped \
+#?           -f 'tag:Name=*' \
+#?           -q "Instances[?not_null(Tags[?Key=='Name'].Value|[0])!=''][PublicIpAddress,Tags[?Key=='Name'].Value|[0]]"
+#?   [
+#?       [
+#?           [
+#?               "54.180.56.131",
+#?               "VPN-2019E-PROD"
+#?           ]
+#?       ]
+#?   ]
 #?
 function desc () {
-    aws ec2 describe-instances --instance-ids "$@"
+    local OPTIND OPTARG opt
+    local -a filters query output
+
+    while getopts f:q:o: opt; do
+        case $opt in
+            f)
+                filters+=("Name=${OPTARG%%=*}")
+                filters+=("Values=${OPTARG#*=}")
+                ;;
+            q)
+                # the selector `|[]` strips the outer layer of `[]` in the result
+                # and if the result list is empty, won't give a literal null.
+                query=(--query "Reservations[*].[$OPTARG]|[]")
+                ;;
+            o)
+                output=(--output "$OPTARG")
+                ;;
+            *)
+                return 255
+                ;;
+        esac
+    done
+
+    if [[ ${#filters} -gt 0 ]]; then
+        filters=(--filters "$(IFS=$','; echo "${filters[*]}")")
+    fi
+    
+    aws "${query[@]}" "${output[@]}" ec2 describe-instances "${filters[@]}"
 }
