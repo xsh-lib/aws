c003343a04249c5d802ded5d2f4548302b0315cf
Alex <alexzhangs@gmail.com>
2019-10-26 16:37:47 +0800

    new feature: iam/user/policy/*

---
+++ functions/iam/user/policy/delete.sh
@@ -0,0 +1,50 @@
+#? Description:
+#?   Delete inline policies for IAM user.
+#?
+#? Usage:
+#?   @delete
+#?     -u USERNAME
+#?     [-n POLICY_NAME] [...]
+#?
+#? Options:
+#?   -u USERNAME
+#?
+#?   The name of the user to associate the policy with.
+#?
+#?   [-n POLICY_NAME] [...]
+#?
+#?   The name of the policy document.
+#?   This option can be used multiple times.
+#?   If not specified, all inline policies for the user are deleted.
+#?
+function delete () {
+    local OPTIND OPTARG opt
+
+    local -a options policy_names
+    while getopts u:n: opt; do
+        case $opt in
+            u)
+                options=(--user-name "$OPTARG")
+                ;;
+            n)
+                policy_names+=$OPTARG
+                ;;
+            *)
+                return 255
+                ;;
+        esac
+    done
+
+    if [[ ${#policy_names[@]} -eq 0 ]]; then
+        xsh log info "collecting attached policies."
+        policy_names=( $(aws --query 'PolicyNames[*]' \
+                             --output text \
+                             iam list-user-policies "${options[@]}") )
+    fi
+
+    local name
+    for name in "${policy_names[@]}"; do
+        xsh log info "$name: deleting policy."
+        aws iam delete-user-policy "${options[@]}" --policy-name "$name"
+    done
+}
---
+++ functions/iam/user/policy/put.sh
@@ -0,0 +1,50 @@
+#? Description:
+#?   Add or update an inline policy for IAM user.
+#?
+#? Usage:
+#?   @put
+#?     -u USERNAME
+#?     -n POLICY_NAME
+#?     -d POLICY_DOCUMENT
+#?
+#? Options:
+#?   -u USERNAME
+#?
+#?   The name of the user to associate the policy with.
+#?
+#?   -n POLICY_NAME
+#?
+#?   The name of the policy document.
+#?
+#?   -d POLICY_DOCUMENT
+#?
+#?   The policy document.
+#?   You must provide policies in JSON format
+#?
+function put () {
+    local OPTIND OPTARG opt
+
+    local username policy_name policy_document
+    while getopts u:n:d: opt; do
+        case $opt in
+            u)
+                username=$OPTARG
+                ;;
+            n)
+                policy_name=$OPTARG
+                ;;
+            d)
+                policy_document=$OPTARG
+                ;;
+            *)
+                return 255
+                ;;
+        esac
+    done
+
+    xsh log info "$policy_name: adding/updating policy for $username."
+    aws iam put-user-policy \
+        --user-name "$username" \
+        --policy-name "$policy_name" \
+        --policy-document "$policy_document"
+}
