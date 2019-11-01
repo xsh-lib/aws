874f471a500459010157fcd3037a0134431ef731
Alex <alexzhangs@gmail.com>
2019-11-02 01:10:00 +0800

    enhancement: s3/bucket/exist: use `s3api` rather than `s3 ls`

--- functions/s3/bucket/exist.sh
+++ functions/s3/bucket/exist.sh
@@ -1,13 +1,28 @@
 #? Description:
 #?   Check the S3 bucket existence.
+#?   The checking is across all regions.
 #?
 #? Usage:
-#?   @exist <BUCKET>
+#?   @exist <NAME>
+#?
+#? Options:
+#?   <NAME>   Bucket name.
 #?
 #? Return:
 #?   0: Exists
-#?   != 0: Dosn't exist
+#?   3: Exists but not accessable
+#?   4: Dosn't exist
+#?   any else: Uncaught error
 #?
 function exist () {
-    aws s3 ls s3://${1:?} >/dev/null 2>&1
+    local name=${1:?}
+    local out
+
+    out=$(aws s3api head-bucket --bucket "$name" 2>&1)
+
+    # remove any non-digit
+    out=${out//[^0-9]/}
+
+    # turn [403, 404] to [3, 4]
+    return $((out % 400))
 }
