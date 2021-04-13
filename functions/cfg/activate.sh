8a9cccb840c7d58aad54f511c06e80070e6cc02f
Alex <alexzhangs@gmail.com>
2021-04-13 21:43:54 +0800

    update cfg/activate.sh

--- functions/cfg/activate.sh
+++ functions/cfg/activate.sh
@@ -5,7 +5,7 @@
 #?   PROFILE  Profile name to activate
 #?
 #? Output:
-#?   Activated profile.
+#?   None.
 #?
 #? @xsh /trap/err -eE
 #? @subshell
@@ -26,11 +26,10 @@ function activate () {
     printf "activating profile: ${profile}\n"
     declare property value n=0
     for value in $(xsh aws/cfg/get "${profile}" \
-                          | column -s, -t \
-                          | awk '{$1=""; print}'); do
+                       | column -s, -t \
+                       | awk '{$1=""; print}'); do
         property=${XSH_AWS_CFG_PROPERTIES[n]#*.}
         aws configure set "default.${property:?}" "${value}"
         n=$((n+1))
     done
-    xsh aws/cfg/list
 }
