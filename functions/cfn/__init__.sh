8de124272d86f55d0f8d62f651425cee08ddd67f
Alex <alexzhangs@gmail.com>
2021-04-13 21:37:53 +0800

    enhancement __init__.sh: add runtime decorator

--- functions/cfn/__init__.sh
+++ functions/cfn/__init__.sh
@@ -4,12 +4,19 @@
 #? This file is sourced while importing any function utility, right before the
 #? function utility was sourced.
 #?
+#? The source of the init file won't happen again on the subsequence calls of
+#? the function utility until it is imported again, except a `runtime` decorator
+#? is used on the init file.
+#?
 #? All variables except those of Array should be exported in order to be
 #? available for the sub-processes.
 #?
 #? The variables of Array can't be exported to the sub-processes due to the
 #? limitation of Bash.
 #? -----------------------------------------------------------------------------
+#?
+#? @runtime
+#?
 
 
 XSH_AWS_CFN__CFG_SUPPORTED_VERSIONS=(
